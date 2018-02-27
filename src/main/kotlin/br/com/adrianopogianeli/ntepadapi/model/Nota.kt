c55b01d5eed9c4f08711d51d8ee23322827bacf7
logonrm <logonrm>
2018-02-26 22:22:19 -0300

    funcionando

--- src/main/kotlin/br/com/adrianopogianeli/ntepadapi/model/Nota.kt
+++ src/main/kotlin/br/com/adrianopogianeli/ntepadapi/model/Nota.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 
-data class Nota(@Id var id: String,
+data class Nota(@Id var id: String? = null,
                 var titulo: String,
                 var conteudo: String
 
